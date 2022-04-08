74ee108117606f98e817949e96eff99c6e35f3ea
kent.chen <kent.chen@hwacom.com>
2022-04-08 14:38:35 +0800

    optimize relationship finding

--- AzureDigitalTwinsTools/Helper/RelationshipHelper.py
+++ AzureDigitalTwinsTools/Helper/RelationshipHelper.py
@@ -2,11 +2,13 @@ import requests
 import uuid
 import json
 from .RequestHelper import RequestHelper
+from .QueryHelper import QueryHelper
 
 class RelationshipHelper(RequestHelper):
 
     def __init__(self, token_path, host_name):
         super().__init__(token_path, host_name)
+        self.__qh = QueryHelper(token_path, host_name)
 
     def update_relationship(self):
         pass
@@ -35,32 +37,21 @@ class RelationshipHelper(RequestHelper):
         return self.request(uri, method)
 
     def find_relationships_with_target(self, dtid, target_dtid, relationship_name=None):
-        resp_content = self.list_relationships(dtid, relationship_name).content
-        relationships = json.loads(resp_content)
-        opt = list()
-
-        for r in relationships['value']:
-            if r['$targetId'] == target_dtid:
-                opt.append({
-                    'relationshipId': r['$relationshipId'],
-                    'relationshipName': r['$relationshipName'],
-                    'sourceId': r['$sourceId'],
-                    'targetId': r['$targetId']
-                })
-
-        return opt
+        return self.__qh.query_relationships(
+            source=dtid, 
+            target=target_dtid, 
+            rname=relationship_name
+        )
 
     def find_and_delete_relationships(self, dtid, relationship_name=None, target_dtid=None):
-        resp_content = self.list_relationships(dtid, relationship_name).content
-        relationships = json.loads(resp_content)
+        rs = self.__qh.query_relationships(
+            source=dtid, 
+            target=target_dtid, 
+            rname=relationship_name
+        )
 
-        if isinstance(target_dtid, str):
-            target_dtid = [target_dtid]
-
-        for r in relationships['value']:
-            if target_dtid != None:
-                for t in target_dtid:
-                    if t == r['$targetId']:
-                        self.delete_relationship(dtid, r['$relationshipId'])
-            else:
-                self.delete_relationship(dtid, r['$relationshipId'])
+        for r in rs:
+            self.delete_relationship(
+                dtid=dtid,
+                relationship_id=r['$relationshipId']
+            )
